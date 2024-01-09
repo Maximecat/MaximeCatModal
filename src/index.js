55fe01a32dff473a2ca623a3a2ea3fbd6be36a28
Maxime CAT <maximecat@outlook.fr>
2024-01-09 13:46:32 +0100

    test modal

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import MaximeCatModal from './lib';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let isOpen = true;
+const handleClose = () => {
+  isOpen = !isOpen
+}
 root.render(
   <React.StrictMode>
     <>
-      <h1>Hello</h1>
-      <MaximeCatModal />
+      <div style={{ position: "relative" }}>
+        <h1>Hello</h1>
+        <MaximeCatModal isOpen={isOpen} onClose={handleClose} message={"Employee Created !"} />
+      </div>
     </>
   </React.StrictMode>
 );
