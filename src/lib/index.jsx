93f9710a41b962fcefb110157a500a47179aece4
Maxime CAT <maximecat@outlook.fr>
2024-01-09 13:46:46 +0100

    html modal

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,5 +1,21 @@
-function MaximeCatModal() {
-    return <p>Hello</p>
+import "./index.css"
+
+function MaximeCatModal({ message, isOpen, onClose }) {
+
+
+    return (
+        <>
+            {
+                isOpen &&
+                <div className="backdrop">
+                    <div className="modal-content">
+                        <p>{message}</p>
+                        <button className="btn-close" onClick={onClose}>x</button>
+                    </div>
+                </div>
+            }
+        </>
+    )
 }
 
 export default MaximeCatModal
