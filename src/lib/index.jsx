26629a22cdacf3d303155ba5bb1da9bb6c526151
Maxime CAT <maximecat@outlook.fr>
2024-01-13 16:45:04 +0100

    add comment modal

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -2,7 +2,15 @@ import "./index.css"
 
 /**
  * @description react modal by maximecat
+ * @param message string to display the message in the modal
+ * @param isOpen boolean that changes from true to false
+ * @param onClose for closure of the modal
+ *
+ * @return void
+ * @author Maxime
+ * @version 1.0
  */
+
 function MaximeCatModal({ message, isOpen, onClose }) {
     return (
         <>
